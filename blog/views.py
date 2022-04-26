f7b9eab813532c7607fef5b73643fb062cb05c9c
Pipsson <juliusmallya8420@gmail.com>
2022-04-26 16:22:03 +0300

    changes on homepage

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def home (request):
-    return render (request, 'blog/home.html')
+
+posts = [
+    {
+        'author': 'Julius',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Mzamburi Jr',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render (request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
